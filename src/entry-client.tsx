4bc3390ead5df4808a3bf514a2fa6240a777823d
Seungpyo Suh <me@sspzoa.io>
2026-09-21 00:37:36 +0900

    feat: add Vercel Analytics

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,3 +1,5 @@
 import { mount, StartClient } from "@solidjs/start/client";
+import { inject } from "@vercel/analytics";
 
+inject({ mode: import.meta.env.DEV ? "development" : "production" });
 mount(() => <StartClient />, document.getElementById("app")!);
